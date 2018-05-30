abe321024cc93fab187fb86e52d613398e626732
julianamarques <julianabmarques@live.com>
2018-05-30 17:15:39 -0300

    Adicionados adapter, Popup menu e layout item para os apartamentos

---
+++ app/src/main/java/com/app/condominioplus/activities/CadastroProprietarioActivity.java
@@ -0,0 +1,15 @@
+package com.app.condominioplus.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.condominioplus.R;
+
+public class CadastroProprietarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_proprietario);
+    }
+}
