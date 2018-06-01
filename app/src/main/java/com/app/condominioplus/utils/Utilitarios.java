c9e5f4a03cb383807a22ed06a1b2f53b03f24ead
LeonardoSalmento <salmento_@hotmail.com>
2018-06-01 10:48:10 -0300

    "Adicionando classe utilitarios"

---
+++ app/src/main/java/com/app/condominioplus/utils/Utilitarios.java
@@ -0,0 +1,17 @@
+package com.app.condominioplus.utils;
+
+import com.app.condominioplus.models.Apartamento;
+
+import java.util.List;
+
+public class Utilitarios {
+
+    public Utilitarios() {
+    }
+
+    /*public void MostrarApartamentos(List<Apartamento> apartamentos){
+        for (int i =0; i<apartamentos.size();i++){
+            apartamentos.get(i);
+        }
+    }*/
+}
